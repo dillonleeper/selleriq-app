990f11a9413ab2c4dd053074928aace68e70e13f
Dillon Leeper <109615972+dillonleeper@users.noreply.github.com>
2026-08-10 15:36:18 -0400

    Remove theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { Analytics } from '@vercel/analytics/next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import './globals.css'
 import AppShell from '@/components/AppShell'
-import { ThemeProvider } from '@/context/ThemeContext'
 
 const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] })
 const geistMono = Geist_Mono({ variable: '--font-geist-mono', subsets: ['latin'] })
@@ -18,11 +17,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body className={geistSans.className}>
-        <ThemeProvider>
-          <AppShell>{children}</AppShell>
-          <Analytics />
-          <SpeedInsights />
-        </ThemeProvider>
+        <AppShell>{children}</AppShell>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
